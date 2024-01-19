51f69b400c4bba9075334145f17064b830010125
PAUKUNO <student.150267@worcesterschools.net>
2024-01-19 02:19:48 -0500

    finished, it plays music now!!

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 //Init Commit
 public class Main {
     public static void main(String[] args) throws IOException, FontFormatException {
-        MyFrame frame = new MyFrame();
+        new MyFrame();
         System.out.print("MAIN DEBUG");
     }
 }
--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,3 +1,8 @@
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -11,16 +16,19 @@ public class MyFrame {
     private int counter = 0;
     private final JLabel label;
 
-
+    private Clip clip;
 
     //Defines Pastel Color Pallete
     private final Color[] colors = {new Color(255, 185, 185), new Color(255, 217, 179), new Color(247, 240, 193)};
+
+    //Defines Path for Audio Files
+    private final String[] soundFiles = {"songs/Animal Crossing Population Growing 5 A.M.wav", "songs/Animal Crossing Population Growing 7 A.M.wav", "songs/Animal Crossing Population Growing 5 P.M.wav"};
 
     public MyFrame() throws IOException, FontFormatException {
         //Makes a button with the text "click here"
         JButton button = new JButton("click here");
         //Makes label for text
-        label = new JLabel("Value: " + counter);
+        label = new JLabel("Click Count: " + counter);
 
         //Loads custom font file from workspace
         File fontFile = new File("assets/mini-wakuwaku.otf");
@@ -37,8 +45,10 @@ public class MyFrame {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Use the font
-        button.setFont(customFont.deriveFont(12f));
-        label.setFont(customFont.deriveFont(12f));
+        button.setFont(customFont.deriveFont(24f));
+        //button.setBorderPainted(false);
+        label.setFont(customFont.deriveFont(24f));
+        label.setBorder(null);
 
         //Centers Button and label
         JPanel panel = new JPanel(new GridBagLayout());
@@ -55,24 +65,56 @@ public class MyFrame {
 
         // Add the panel to the center of the frame
         frame.add(panel, BorderLayout.CENTER);
-
-
+        
+        //label.setForeground(panel.getBackground());
 
 
 
         button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
+        button.setFocusPainted(false);
 
         // Add Action Listener
+        
         button.addActionListener(new ActionListener() {
-            int colorIndex = 0; // Initialize color index
+            int colorIndex = 0; // Make color index
+            int soundIndex = 0; // Make sound index
             public void actionPerformed(ActionEvent e) {
                 counter++;
-                label.setText("Value: " + counter);
+                label.setText("Click Count: " + counter);
                 panel.setBackground(colors[colorIndex]);
+                button.setBackground(colors[colorIndex]);
+                stopSound();
+                playSound(soundFiles[soundIndex]);
+
                 colorIndex = (colorIndex + 1) % colors.length;
+                soundIndex = (soundIndex + 1) % soundFiles.length;
             }
         });
 
         frame.setVisible(true); // Display the frame
     }
+
+
+
+     // Play the sound file
+    private void playSound(String soundFile) {
+        try {
+            File file = new File(soundFile);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        } catch (LineUnavailableException | UnsupportedAudioFileException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void stopSound() {
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+        }
+    }
+
+
+
 }
